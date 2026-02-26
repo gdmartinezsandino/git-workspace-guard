76c5ad7ef4702537c8f8f9a52c65799da2e6f7a0
Gabriel Martinez Sandino <gdmartinez.sandino@gmail.com>
2026-02-26 17:48:09 -0500

    chore: update formula to v1.0.3

--- Formula/git-workspace-guard.rb
+++ Formula/git-workspace-guard.rb
@@ -1,9 +1,9 @@
 class GitWorkspaceGuard < Formula
   desc "Identity Firewall for devs managing multiple Git identities"
   homepage "https://github.com/gdmartinezsandino/git-workspace-guard"
-  url "https://github.com/gdmartinezsandino/git-workspace-guard/archive/refs/tags/v1.0.2.tar.gz"
-  # curl -sL https://github.com/gdmartinezsandino/git-workspace-guard/archive/refs/tags/v1.0.0.tar.gz | shasum -a 256
-  sha256 "547cfa114b65444ded795add29aa0c174ea43043792f914ce24ea6969e8eff69"
+  url "https://github.com/gdmartinezsandino/git-workspace-guard/archive/refs/tags/v1.0.3.tar.gz"
+  # curl -sL https://github.com/gdmartinezsandino/git-workspace-guard/archive/refs/tags/v1.0.3.tar.gz | shasum -a 256
+  sha256 "3ed8421d014369e22bed0b1ba11f975981b24be7691b55871b5d422549d1324e"
   license "ISC"
 
   depends_on "node@22"
@@ -21,6 +21,6 @@ class GitWorkspaceGuard < Formula
   end
 
   test do
-    assert_match "1.0.2", shell_output("#{bin}/gw --version")
+    assert_match "1.0.3", shell_output("#{bin}/gw --version")
   end
 end
